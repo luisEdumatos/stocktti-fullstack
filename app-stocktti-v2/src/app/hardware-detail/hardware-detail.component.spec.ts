8c2ab978ae3d0d582966cad77874ab9270ec4db0
luis.eduardo <luisedu.unifal@gmail.com>
2021-08-27 11:12:43 -0300

    update angular 12

--- app-stocktti-v2/src/app/hardware-detail/hardware-detail.component.spec.ts
+++ app-stocktti-v2/src/app/hardware-detail/hardware-detail.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { HardwareDetailComponent } from './components/hardware-detail.component';
 
@@ -6,7 +6,7 @@ describe('HardwareDetailComponent', () => {
   let component: HardwareDetailComponent;
   let fixture: ComponentFixture<HardwareDetailComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HardwareDetailComponent ]
     })
--- app-stocktti-v2/src/app/hardware/hardware.component.spec.ts
+++ app-stocktti-v2/src/app/hardware/hardware.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { HardwareComponent } from './components/hardware.component';
 
@@ -6,7 +6,7 @@ describe('HardwareComponent', () => {
   let component: HardwareComponent;
   let fixture: ComponentFixture<HardwareComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HardwareComponent ]
     })
